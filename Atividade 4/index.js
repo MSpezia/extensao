7f41fe273cb68811c301c5d6e18d8223de186726
MSpezia <79917270+MSpezia@users.noreply.github.com>
2023-09-06 09:59:26 -0300

    Add files via upload

--- Atividade 4/index.js
+++ Atividade 4/index.js
@@ -10,6 +10,30 @@ const express = require("express");
         res.send("Hello World");
     })
 
+app.get("/somar/:n1/:n2", function(req,res){
+    let n1 = req.params.n1;
+    let n2 = req.params.n2;
+    res.send(`${n1} + ${n2} = ${calc.somar(n1,n2)}`);
+})
+
+app.get("/subtrair/:n1/:n2", function(req,res){
+    let n1 = req.params.n1;
+    let n2 = req.params.n2;
+    res.send(`${n1} - ${n2} = ${calc.subtrair(n1,n2)}`);
+})
+
+app.get("/multiplicar/:n1/:n2", function(req,res){
+    let n1 = req.params.n1;
+    let n2 = req.params.n2;
+    res.send(`${n1} * ${n2} = ${calc.multiplicar(n1,n2)}`);
+})
+
+app.get("/dividir/:n1/:n2", function(req,res){
+    let n1 = req.params.n1;
+    let n2 = req.params.n2;
+    res.send(`${n1} / ${n2} = ${calc.dividir(n1,n2)}`);
+})
+
 const PORT = 8080;
 app.listen(PORT, function (){
     console.log("app rodando na porta " + PORT);
